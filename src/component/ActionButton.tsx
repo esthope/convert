bc6ba17fd7eab7a2686dc409e2cc245e24e3247c
Lucile <esthope.42@outlook.fr>
2025-04-28 19:07:02 +0200

    [ANA] deploy

--- src/component/ActionButton.tsx
+++ src/component/ActionButton.tsx
@@ -21,22 +21,14 @@ const ActionButton = ({entry, label, onMouseEnter, statut, onClick}:ActionProp):
 	const [IconPath, setIconPath] = useState<any>(CircleIcon);
 
 	let buttonProp:object = {},
-		imageProp:object = {},
-		unmounted = useRef(true);
+		imageProp:object = {};
 
 	if (onClick) buttonProp = {onClick: onClick};
 	if (onMouseEnter) imageProp = {onMouseEnter: onMouseEnter};
 
 	// get the button icon
 	useEffect(()=>{
-		console.log(unmounted?.current, entry)
-		if (unmounted?.current) {
-			unmounted.current = false
-			return 
-		}
-	
-		console.log(entry)
-		console.log(CopyIcon)
+		console.log(label)
 		switch (entry) {
 			case 'copy':
 				setIconPath(CopyIcon)
@@ -53,8 +45,6 @@ const ActionButton = ({entry, label, onMouseEnter, statut, onClick}:ActionProp):
 			default:
 				setIconPath(CircleIcon)
 		}
-
-		return () => {unmounted.current = true}
 	}, [entry])
 
 	return (
